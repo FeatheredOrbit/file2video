8db2bb9be2ae8cb6a107890dd90fd4d14724eb4a
Feathered Orbit <featheredorbit.dev@gmail.com>
2026-06-07 23:00:10 +0100

    I decided to use ffmpeg with std::process::Commands, any crate I find are crap. I  built a function that checks the presence of ffmpeg at startup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 
 mod args;
 mod process;
@@ -6,8 +6,26 @@ mod process;
 use args::Args;
 
 fn main() {
+    if !ffmpeg_is_installed() {
+
+        Args::command()
+            .error(clap::error::ErrorKind::Io, "ffmpeg is not installed on the device.")
+            .exit()
+
+    }
+
     let args = Args::parse();
 
     process::process(args);
+
+}
+
+fn ffmpeg_is_installed() -> bool {
+
+    std::process::Command::new("ffmpeg")
+        .arg("-version")
+        .output()
+        .map(|output| output.status.success())
+        .unwrap_or(false)
 
 }
